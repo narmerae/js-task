3147a6aa305793584198012e51501c830fd47edb
Narmi <kujundzicmatej64@gmail.com>
2024-07-18 18:32:05 +0200

    feat(driverSearch): properly working

---
+++ script.js
@@ -0,0 +1,63 @@
+
+async function getDriver() {
+    let numberDriver = document.getElementById("searchbar").value
+    const url = `https://api.openf1.org/v1/drivers?driver_number=${numberDriver}&session_key=9158`
+    try {
+        const response = await fetch(url);
+        
+        if(!response.ok) {
+            throw new Error(`Response status is ${response.status}`);
+        }
+        const data = await response.json();
+        
+        switch (data[0].driver_number) {
+            case 14:
+                document.getElementById("easterEgg").src = "https://www.youtube.com/embed/0Y2PPxrBJt8?si=M7Z8g18bXEoclfi5"
+                document.getElementById("easterText").innerText = "VAMOOOOSSSSSSS!!!!!"
+                break;
+            case 1:
+                document.getElementById("easterEgg").src= "https://www.youtube.com/embed/BLdVvIwUXK4?si=n6IE87uufaDKb3L_"
+                document.getElementById("easterText").innerText = "SID 🤓"
+            default:
+                break;
+        }
+        
+        document.getElementById("imageDriver").src = data[0].headshot_url;
+        document.getElementById("imageDriver").style.display = "block";
+        document.getElementById("nameDriver").innerText = data[0].full_name
+    }
+    catch (error) {
+        console.error("Error is " + error.message);
+        alert("Driver doesn't exist");
+    }
+}
+
+async function getDriverList() {
+    const url = "https://api.openf1.org/v1/drivers"
+    try {
+        const response = await fetch(url)
+        if(!response.ok) {
+            throw new Error(`Response status is ${response.status}`)
+        }
+        const data = await response.json();
+        console.log(data)
+        
+        let table = document.getElementById("driverTable")
+        
+        for (let i = 0; i < 25; i++){
+            let row = `<tr>
+                                   <td>${data[i].driver_number}</td>
+                                   <td>${data[i].full_name}</td>
+                                   <td>${data[i].team_name}</td>
+                                   <td><></td>
+                              </tr>
+                                  `
+            table.innerHTML += row;
+        }
+    }
+    catch (error) {
+        console.error("Error is " + error.message);
+        alert("Something went wrong!");
+    }
+}
+
